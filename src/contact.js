3cba53beeb9d4f80382285a07163758620400ce0
Shelly Leung <pontificable@yahoo.com>
2018-12-10 22:44:07 -0800

    css for contact form

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './contact.css';
 
 export default function ContactForm(props) {
 	return (
 		<div className="contact">
 			<form>
+				<input type="text" id="name" name="name" placeholder="Your Name" />
 				<input type="text" id="email" name="email" placeholder="Your Email" />
 				<select id="subject" name="subject">
-					<option value="performance">Performance</option>
-					<option value="props">Fire Props</option>
+					<option value="performance">Book Me for a Fire Performance</option>
+					<option value="props">Need Some Fire Props?</option>
 					<option value="misc">Other</option>
 				</select>
 				<textarea
